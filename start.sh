87c83f291a315b1f6fa3b263dd99840a08a8947b
Kazeem Odutola <kazeemodutola@pc-130-230.customer.ask4.lan>
2023-01-29 11:46:12 +0000

    exit test with zero

--- start.sh
+++ start.sh
@@ -4,7 +4,5 @@ set -e
 echo "run db migration"
 /app/migrate -path /app/db/migration -database "$DB_SOURCE" -verbose up
 
-# migrate -path db/migration -database "postgresql://postgres:secret@postgres:5432/video-manager?sslmode=disable" -verbose up
-
 echo "start the app"
 exec "$@"
